bb2ee04faac63d6620c87ce80d2f2e351b9564c1
Karol Lobos <karollobos@interia.pl>
2020-01-18 20:52:08 +0100

    Edit showing Node version

--- program.js
+++ program.js
@@ -4,11 +4,13 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        console.log('Your NODE.js version is: ' + process.version);
         switch (instruction) {
             case '/exit':
                 process.stdout.write('Quitting APP!\n');
                 process.exit();
+                break;
+            case '/version':
+                console.log('Your NODE.js version: ' + process.version);
                 break;
             default:
                 process.stderr.write('Wrong instruction!\n');
